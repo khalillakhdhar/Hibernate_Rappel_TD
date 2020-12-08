06a8a61c5bc1b1331bbba0bffc3f4d6eb73f939a
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-08 10:20:01 +0100

    Create Rappel.java

---
+++ src/main/java/com/hibernate/model/Rappel.java
@@ -0,0 +1,48 @@
+package com.hibernate.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Rappel {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	String titre;
+	@Column(nullable = true)
+	String description;
+	@Column(nullable = false)
+	String type;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitre() {
+		return titre;
+	}
+	public void setTitre(String titre) {
+		this.titre = titre;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+	
+}
