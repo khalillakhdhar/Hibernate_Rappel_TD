c0fa0ca3f0be078f9574116549db2fa936f82d98
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2020-12-08 10:17:26 +0100

    Update Activite.java

--- src/main/java/com/hibernate/model/Activite.java
+++ src/main/java/com/hibernate/model/Activite.java
@@ -1,5 +1,43 @@
 package com.hibernate.model;
 
-public class Activite {
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+import org.springframework.boot.context.properties.bind.DefaultValue;
+
+import com.sun.istack.NotNull;
+
+@Entity
+public class Activite {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	@Column(nullable = false)
+	//@DefaultValue("loisir")
+	String type;
+	@NotNull
+	String description;
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+	
 }
